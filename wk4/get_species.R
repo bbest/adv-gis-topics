2d431a3a4ee34ebe6d88055bcd06e6cb9de7f518
unknown <bbest_student@BOLLWEEVIL.esm.ucsb.edu>
2014-10-24 17:02:06 -0700

    changed species to one with only ~100 records (vs ~24K), set CRS to WGS84, explained warning

--- wk4/get_species.R
+++ wk4/get_species.R
@@ -1,10 +1,10 @@
-library(dismo)
+library(sp)
 library(rgdal)
-library(raster)
+library(dismo)
 
 # set variables to the scientific name of your speciess
-genus   = 'Megaptera'
-species = 'novaeangliae'
+genus   = 'Pinus'
+species = 'longaeva'
 
 # path to output shapefile constructed from scientific name of your speciess
 shp = sprintf('H:/esm296-4f/project/data/gbif_%s_%s', genus, species)
@@ -12,8 +12,16 @@ shp = sprintf('H:/esm296-4f/project/data/gbif_%s_%s', genus, species)
 # get data from Global Biodiversity Information Facility (GBIF)
 sp = gbif(genus, species, sp=T, removeZeros=T)
 
+# set geographic coordinate system
+proj4string(sp) = CRS('+proj=longlat +datum=WGS84')
+
 # make folder if doesn't exist
 dir.create(dirname(shp), showWarnings=F, recursive=T)
 
 # write to shapefile
 writeOGR(sp, dsn=dirname(shp), layer=basename(shp), driver='ESRI Shapefile')
+# You'll get the following message, which is OK. Just a limitation of the shapefile format: only 10 characters.
+#
+# Warning message:
+# In writeOGR(sp, dsn = dirname(shp), layer = basename(shp), driver = "ESRI Shapefile") :
+#   Field names abbreviated for ESRI Shapefile driver
